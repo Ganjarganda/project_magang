c4ca6ea70158c48d3c27eb0d3027d7ca461f5d14
Ganjarganda <32573348+Ganjarganda@users.noreply.github.com>
2024-08-08 15:52:41 +0700

    update login

--- lib/controller/user/user_controller.dart
+++ lib/controller/user/user_controller.dart
@@ -2,4 +2,8 @@ import 'package:get/get.dart';
 
 class UserController extends GetxController {
   RxBool hidePassword = true.obs;
+
+  RxBool validEmail = true.obs;
+  RxBool validPassword = true.obs;
+
 }
