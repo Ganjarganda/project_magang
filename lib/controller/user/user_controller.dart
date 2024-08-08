72aaef805cfdf8fe85c1293bac50d4155f0ee255
PutiAndini <130969621+PutiAndini@users.noreply.github.com>
2024-08-08 16:52:05 +0700

    upgrade ui register

--- lib/controller/user/user_controller.dart
+++ lib/controller/user/user_controller.dart
@@ -3,7 +3,10 @@ import 'package:get/get.dart';
 class UserController extends GetxController {
   RxBool hidePassword = true.obs;
 
+
+  RxBool validNamaLengkap = true.obs;
   RxBool validEmail = true.obs;
   RxBool validPassword = true.obs;
+  RxBool validConfimPass = true.obs;
 
 }
